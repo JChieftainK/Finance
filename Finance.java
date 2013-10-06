199e11bd4d59613a939a9c85e34895514c723b11
JChieftainK <jkennedy11@Justins-Macbook-Pro.local>
2013-10-06 17:37:27 -0500

    Added Comments

--- Finance.java
+++ Finance.java
@@ -13,30 +13,30 @@ public class Finance extends JFrame implements ActionListener{
 	private JMenuItem menuItem;
 	
 	public Finance(){ //Default Constructor
-		super("Budget Using Graphs & Numbers");
-		contents = getContentPane();
-		contents.setLayout(new BorderLayout());
+		super("Budget Using Graphs & Numbers"); //Used to change title of window
+		contents = getContentPane(); 
+		contents.setLayout(new BorderLayout()); //Set the main window to BorderLayout
 		
 		//*************************************
 		menuBar = new JMenuBar(); //Set up JMenu
 		
 		menu = new JMenu("File"); //Set up "File" for JMenu
-		menuBar.add(menu);
+		menuBar.add(menu); //Add the "File" option
 			
 		menuItem = new JMenuItem("Exit"); //Add "Exit" to "File" menu
-		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
-		menuItem.addActionListener(this);
-		menu.add(menuItem);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK)); //Type control + E to call
+		menuItem.addActionListener(this); //Add a listener to the object
+		menu.add(menuItem); //Add the "Exit"
 		
 		setJMenuBar(menuBar); //Adds menu bar to GUI
 		//*************************************
 		
-		setSize(200, 200);
-		setVisible(true);
+		setSize(200, 200); //Changes the size of the window by pixels
+		setVisible(true); //Tells it to be seen on the computer screen
 	}
 	
-	public void actionPerformed(ActionEvent ae){
-		if(ae.getSource() instanceof JMenuItem){
+	public void actionPerformed(ActionEvent ae){ //Method that is called for Items that have action listeners
+		if(ae.getSource() instanceof JMenuItem){ //Checks the instance of the listener for Menu stuff
 			JMenuItem source = (JMenuItem)(ae.getSource());
 			if(source.getText() == "Exit"){ //Exit clicked. Exit program
 				System.exit(0);
@@ -45,7 +45,7 @@ public class Finance extends JFrame implements ActionListener{
 	}
 	
 	public static void main(String[] args) { //Main method starts program
-		Finance fin = new Finance();
-		fin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Finance fin = new Finance(); //Create an object of the finance app
+		fin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set default close operation to the normal exit on close
 	}
 }
