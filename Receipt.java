dd843a9111e2f702d88cf4a6339fce4f732897e5
JChieftainK <jkennedy11@justins-mbp.msbu.stu.wifi.okcu.edu>
2013-11-04 11:19:31 -0600

    Added Receipt and Testing Receipt

---
+++ Receipt.java
@@ -0,0 +1,19 @@
+public class Receipt implements ReceiptInterface {
+	
+}
+
+/*public void setWhere(String newEntry);
+public void setDay(int day);
+public void setMonth(int month);
+public void setYear(int year);
+public void setCost(double cost);
+
+public String getWhere();
+public int getDay();
+public int getMonth();
+public int getYear();
+public int getCost();
+
+public String getInfo();
+public String getDate();
+public boolean equals(Receipt testEntry);*/
---
+++ TestReceipt.java
@@ -0,0 +1,5 @@
+public class TestReceipt {
+	public static void main(String[] args) {
+		
+	}
+}
